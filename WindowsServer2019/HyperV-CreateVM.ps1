f932014c800d824c018e821475b1c6499c74f270
apple <apple@apples-MacBook-Air.local>
2021-08-03 20:42:15 +0800

    Add HyperV-CreateVM.ps1

--- WindowsServer2019/HyperV-CreateVM.ps1
+++ WindowsServer2019/HyperV-CreateVM.ps1
@@ -1,18 +1,11 @@
-﻿#TIADC1PDLP01	192.168.103.70	Internal	Production	Primary	>= 2G	8	32	300	600	SAN Storage
-
-$VMname = 'New Virtual Machine'
+﻿$VMname = 'New Virtual Machine'
 $Memory = 32GB
 $vCPU = 8
 $GoldenImageDir = ""
 $VMStorage = ""
 
-# Interactive Mode
-# $VMname = Read-Host -Prompt "VMname"
-
-
 # Create a New VM
 New-VM -Name $VMname -SwitchName extvswitch -NoVHD -Generation 2 -MemoryStartupBytes $Memory -Path C:\ClusterStorage\Volume1\Hyper-V\
-
 
 # Copy Syspreped Golden Image
 ## Create a Virtual Disk Folder
@@ -28,7 +21,6 @@ $Destination = "C:\ClusterStorage\Volume1\Hyper-V\$VMname\Virtual Hard Disks\$VM
 Start-BitsTransfer `
 -Source 'C:\source\Hyper-V Golden Image\GoldenImage.vhdx' `
 -Destination $Destination
-
 
 # Attached the Golden Image to VM
 $Path = "C:\ClusterStorage\Volume1\Hyper-V\$VMname\Virtual Hard Disks\$VMname"+"_disk1.vhdx" 
