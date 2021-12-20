efb64a07edb7116c469ceea54f7c9b4dd70de98b
ITsLoveCats <67352969+ITsLoveCats@users.noreply.github.com>
2021-12-20 22:59:54 +0800

    ConfigureOS FirstCommit

--- ConfigureOS.ps1
+++ ConfigureOS.ps1
@@ -1,17 +1,20 @@
 Start-Transcript
 
-$computerName= "Virtual Machine Name"
+# Declare variables
+$computerName= "your_VMName"
 $ipAddress= "192.168.103.59"
 $ipPrefix= "24"
 $ipGW= "192.168.103.1"
 $ipDNS= ("192.168.103.31", "192.168.103.32","192.168.4.31", "8.8.8.8")
 $ipIF= (Get-NetAdapter).ifIndex
 $ipv6 = (Get-NetAdapterBinding -ComponentID ms_tcpip6).ifAlias
+$featureLogPath = "c:\poshlog\featurelog.txt" 
+$addsTools = "" 
 
+# Set Time Zone
 Write-Verbose 'Set Timezone & Usage Location' -Verbose
 Set-TimeZone -Id 'China Standard Time'
 Set-WinHomeLocation -GeoId 104
-
 
 # Before Setting Static IP, Dhcp must be disabled first
 Write-Verbose 'Disable Dhcp' -Verbose
@@ -21,7 +24,6 @@ Set-ItemProperty -Path “HKLM:\SYSTEM\CurrentControlSet\services\Tcpip\Paramete
 # Now Setting Static IP
 Write-Verbose 'Setting Static IP' -Verbose
 New-NetIPAddress -InterfaceIndex $ipIF -IPAddress $ipAddress -PrefixLength $ipPrefix -DefaultGateway $ipGW 
-Get-NetIPAddress -ifIndex $ipIF -AddressFamily IPv4
 
 # Now Setting DNS Server IP
 Write-Verbose 'Setting DNS Server Address' -Verbose
@@ -35,6 +37,11 @@ Disable-NetAdapterBinding -Name $ipv6 -ComponentID ms_tcpip6
 Write-Verbose 'Enable Remote Desktop' -Verbose
 Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" –Value 0
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+
+# Install features
+New-Item $featureLogPath -ItemType file -Force 
+Add-WindowsFeature $addsTools 
+Get-WindowsFeature | Where installed >>$featureLogPath 
 
 # Now Renaming Computer
 Write-Verbose 'Renaming Computer' -Verbose
